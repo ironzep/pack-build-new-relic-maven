61a760f2dcc11572c67089583702a769a89906bd
ironzep <renan_geraldo@yahoo.com.br>
2022-05-31 18:58:30 -0300

    First Commit

---
+++ src/main/java/com/example/packbuildnewrelic/PackBuildNewRelicApplication.java
@@ -0,0 +1,13 @@
+package com.example.packbuildnewrelic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackBuildNewRelicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackBuildNewRelicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/packbuildnewrelic/PackBuildNewRelicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.packbuildnewrelic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PackBuildNewRelicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
